4f05722bb437d1cb771a6ef67f446d26a2d1f2bf
pshrote-afk <pshrote1@gmail.com>
2025-11-14 11:13:32 +0530

    #67 remove dl and pl folders from hr/

--- hr/dl/src/com/thinking/machines/hr/dl/exceptions/DAOException.java
+++
@@ -1,8 +0,0 @@
-package com.thinking.machines.hr.dl.exceptions;
-
-public class DAOException extends Exception // for Checked Exception
-{
-  public DAOException(String message) {
-    super(message);
-  }
-}

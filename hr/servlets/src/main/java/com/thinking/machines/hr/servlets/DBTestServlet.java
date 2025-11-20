0ff8be1935dc17f24b82812102c26d4605f1f6c8
pshrote-afk <pshrote1@gmail.com>
2025-11-21 00:29:51 +0530

    #90 add database test endpoint

---
+++ hr/servlets/src/main/java/com/thinking/machines/hr/servlets/DBTestServlet.java
@@ -0,0 +1,36 @@
+package com.thinking.machines.hr.servlets.courses;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import com.thinking.machines.hr.dl.exceptions.*;
+import com.thinking.machines.hr.dl.dao.*;
+
+public class DBTestServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+	try
+	{
+	Connection connection = DAOConnection.getConnection();
+
+            response.getWriter().println("DB Connected: " + !connection.isClosed());
+        } catch(DAOException dao)
+	{
+		System.out.println(dao.getMessage());
+	}
+
+	catch (SQLException e) {
+            e.printStackTrace(response.getWriter());
+        }
+	catch(Exception e)
+	{
+	e.printStackTrace(response.getWriter());
+	}
+
+    }
+}
